c84af382f6961538c751666b523d2d5841dc1f1b
Nicolás <nico.alan.alb@gmail.com>
2022-11-23 17:44:39 -0300

    Added explicit intents to open a new activity when clicking a list.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,7 +15,9 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view) {
+        Intent openNumbersList = new Intent(this, NumbersActivity.class);
+        startActivity(openNumbersList);
+    }
+
+    public void openFamilyList(View view) {
+        Intent openFamilyList = new Intent(this, FamilyActivity.class);
+        startActivity(openFamilyList);
+    }
+
+    public void openColorsList(View view) {
+        Intent openColorsList = new Intent(this, ColorsActivity.class);
+        startActivity(openColorsList);
+    }
+
+    public void openPhrasesList(View view) {
+        Intent openPhrasesList = new Intent(this, PhrasesActivity.class);
+        startActivity(openPhrasesList);
     }
 }
