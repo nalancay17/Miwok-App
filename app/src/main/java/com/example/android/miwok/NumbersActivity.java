5490b57ec637ad2815309de5895455c1f12af94f
Nicolás <nico.alan.alb@gmail.com>
2022-11-23 19:38:41 -0300

    LinearLayout in activity_numbers was replaced by a ListView and now the addition of words is done by using an ArrayAdapter. These changes improve app performance.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,20 +18,14 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        List<String> words = getWordsCollection();
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        addWordsViews(words, rootView);
+        List<String> words = createWordsCollection();
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = findViewById(R.id.numbersList);
+        listView.setAdapter(itemsAdapter);
     }
 
-    private void addWordsViews(List<String> words, LinearLayout rootView) {
-        for(String word: words) {
-            TextView wordView = new TextView(this);
-            wordView.setText(word);
-            rootView.addView(wordView);
-        }
-    }
-
-    private List<String> getWordsCollection() {
+    private List<String> createWordsCollection() {
         ArrayList<String> numbers = new ArrayList<>();
         numbers.add("one");
         numbers.add("two");
