15ed8afbcf3f63c3016791ac7ab23fc94369b0e2
Nicolás <nico.alan.alb@gmail.com>
2022-11-23 23:51:11 -0300

    Modification in WordAdapter getView method to load images in each word of the lists. Phrases list needs to be fixed.

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -34,6 +35,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
 
         setTexts(word, convertView);
+        setImage(word, convertView);
 
         // Return the completed view to render on screen
         return convertView;
@@ -47,6 +49,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Populate the data into the template view using the data object
         miwokTranslation.setText(word.getMiwokTranslation());
         defaultTranslation.setText(word.getDefaultTranslation());
+    }
+
+    private void setImage(Word word, View convertView) {
+        ImageView itemImage = convertView.findViewById(R.id.item_image);
+        itemImage.setImageResource(word.getImageResourceId());
     }
 
 }
