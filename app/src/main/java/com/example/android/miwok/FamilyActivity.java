af3bb2d94495d629222f297dda496076c729565c
Nicolás <nico.alan.alb@gmail.com>
2022-12-18 00:35:53 -0300

    Audio focus management implementation in family activity.

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,7 +2,11 @@ package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
+import android.media.AudioFocusRequest;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -12,13 +16,29 @@ import java.util.List;
 public class FamilyActivity extends AppCompatActivity {
 
     private MediaPlayer player;
-    private MediaPlayer.OnCompletionListener completionListener = completion -> releaseMediaPlayer();
+    private final MediaPlayer.OnCompletionListener completionListener = completion -> releaseMediaPlayer();
+
+    private AudioManager audioManager;
+    private AudioFocusRequest focusRequest;
+    private final AudioAttributes attributes = new AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build();
+    private final AudioManager.OnAudioFocusChangeListener focusChangeListener = focusChange -> {
+        if (focusChange == AudioManager.AUDIOFOCUS_GAIN)
+            player.start();
+        else if (focusChange == AudioManager.AUDIOFOCUS_LOSS)
+            releaseMediaPlayer();
+        else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
+            player.pause();
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
         List<Word> words = createFamilyCollection();
 
         WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_family);
@@ -28,11 +48,13 @@ public class FamilyActivity extends AppCompatActivity {
         listView.setOnItemClickListener((adapterView, view, position, id) -> {
             Word currentWord = words.get(position);
 
-            // Release player if it currently exits to play a different sound file
-            releaseMediaPlayer();
-            player = MediaPlayer.create(FamilyActivity.this, currentWord.getAudioResourceId());
-            player.start();
-            player.setOnCompletionListener(completionListener);
+            if (makeFocusRequest() == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                // Release player if it currently exits to play a different sound file
+                releaseMediaPlayer();
+                player = MediaPlayer.create(FamilyActivity.this, currentWord.getAudioResourceId());
+                player.start();
+                player.setOnCompletionListener(completionListener);
+            }
         });
     }
 
@@ -41,6 +63,20 @@ public class FamilyActivity extends AppCompatActivity {
         super.onStop();
         // Media player resource releasing when activity enters in stopped state
         releaseMediaPlayer();
+    }
+
+    private int makeFocusRequest() {
+        // request for api >= 26
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            focusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
+                    .setAudioAttributes(attributes)
+                    .setOnAudioFocusChangeListener(focusChangeListener)
+                    .build();
+            return audioManager.requestAudioFocus(focusRequest);
+        }
+        // request for api < 26. deprecated but needed
+        else
+            return audioManager.requestAudioFocus(focusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
     }
 
     /**
@@ -53,6 +89,12 @@ public class FamilyActivity extends AppCompatActivity {
             // For our code, we've decided that setting the media player to null is an easy way to
             // tell that the media player is not configured to play an audio file at the moment.
             player = null;
+
+            // Abandon audio focus depending on device version
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                audioManager.abandonAudioFocusRequest(focusRequest);
+            else
+                audioManager.abandonAudioFocus(focusChangeListener);
         }
     }
 
