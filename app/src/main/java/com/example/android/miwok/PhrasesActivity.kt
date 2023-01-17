4dc94a26f39fd92e11d2c2600e1e17530ccacf1e
Nicolás <nico.alan.alb@gmail.com>
2023-01-17 01:21:23 -0300

    PhrasesActivity code migration from java to kotlin.

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.kt
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.kt
@@ -1,115 +1,113 @@
-package com.example.android.miwok;
+package com.example.android.miwok
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
+import android.media.AudioManager.OnAudioFocusChangeListener
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnCompletionListener
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
-import android.media.AudioAttributes;
-import android.media.AudioFocusRequest;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.os.Build;
-import android.os.Bundle;
-import android.widget.ListView;
+class PhrasesActivity : AppCompatActivity() {
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class PhrasesActivity extends AppCompatActivity {
-
-    private MediaPlayer player;
-    private final MediaPlayer.OnCompletionListener completionListener = completion -> releaseMediaPlayer();
-
-    private AudioManager audioManager;
-    private AudioFocusRequest focusRequest;
-    private final AudioAttributes attributes = new AudioAttributes.Builder()
-            .setUsage(AudioAttributes.USAGE_MEDIA)
-            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-            .build();
-    private final AudioManager.OnAudioFocusChangeListener focusChangeListener = focusChange -> {
-        if (focusChange == AudioManager.AUDIOFOCUS_GAIN)
-            player.start();
-        else if (focusChange == AudioManager.AUDIOFOCUS_LOSS)
-            releaseMediaPlayer();
-        else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
-            player.pause();
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-        List<Word> words = createPhrasesCollection();
-
-        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_phrases);
-        ListView listView = findViewById(R.id.wordsList);
-        listView.setAdapter(itemsAdapter);
-
-        listView.setOnItemClickListener((adapterView, view, position, id) -> {
-            Word currentWord = words.get(position);
-
-            if (makeFocusRequest() == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                // Release player if it currently exits to play a different sound file
-                releaseMediaPlayer();
-                player = MediaPlayer.create(PhrasesActivity.this, currentWord.getAudioResourceId());
-                player.start();
-                player.setOnCompletionListener(completionListener);
-            }
-        });
+    private lateinit var player: MediaPlayer
+    private val completionListener = OnCompletionListener {
+        releaseMediaPlayer()
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // Media player resource releasing when activity enters in stopped state
-        releaseMediaPlayer();
-    }
-
-    private int makeFocusRequest() {
-        // request for api >= 26
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            focusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
-                    .setAudioAttributes(attributes)
-                    .setOnAudioFocusChangeListener(focusChangeListener)
-                    .build();
-            return audioManager.requestAudioFocus(focusRequest);
+    private lateinit var audioManager: AudioManager
+    private lateinit var focusRequest: AudioFocusRequest
+    private val attributes = AudioAttributes.Builder()
+        .setUsage(AudioAttributes.USAGE_MEDIA)
+        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+        .build()
+    private val focusChangeListener = OnAudioFocusChangeListener { focusChange: Int ->
+        when (focusChange) {
+            AudioManager.AUDIOFOCUS_GAIN -> player.start()
+            AudioManager.AUDIOFOCUS_LOSS -> releaseMediaPlayer()
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT, AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> player.pause()
         }
-        // request for api < 26. deprecated but needed
-        else
-            return audioManager.requestAudioFocus(focusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.word_list)
+
+        audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        val words = createPhrasesCollection()
+
+        val itemsAdapter = WordAdapter(this, words, R.color.category_phrases)
+        val listView = findViewById<ListView>(R.id.wordsList)
+        listView.adapter = itemsAdapter
+
+        listView.onItemClickListener =
+            OnItemClickListener { _: AdapterView<*>?, _: View?, position: Int, _: Long ->
+                val currentWord = words[position]
+                if (makeFocusRequest() == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // Release player if it currently exits to play a different sound file
+                    releaseMediaPlayer()
+                    player = MediaPlayer.create(this@PhrasesActivity, currentWord.audioResourceId)
+                    player.start()
+                    player.setOnCompletionListener(completionListener)
+                }
+            }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // Media player resource releasing when activity enters in stopped state
+        releaseMediaPlayer()
+    }
+
+    private fun makeFocusRequest(): Int {
+        // request for api >= 26
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            focusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
+                .setAudioAttributes(attributes)
+                .setOnAudioFocusChangeListener(focusChangeListener)
+                .build()
+            audioManager.requestAudioFocus(focusRequest)
+        } else {
+            audioManager.requestAudioFocus(
+                focusChangeListener,
+                AudioManager.STREAM_MUSIC,
+                AudioManager.AUDIOFOCUS_GAIN_TRANSIENT
+            )
+        }
     }
 
     /**
      * Clean up the media player by releasing its resources.
      */
-    private void releaseMediaPlayer() {
-        if (player != null) {
-            player.release();
+    private fun releaseMediaPlayer() {
+        if (this::player.isInitialized)
+            player.release()
 
-            // For our code, we've decided that setting the media player to null is an easy way to
-            // tell that the media player is not configured to play an audio file at the moment.
-            player = null;
-
-            // Abandon audio focus depending on device version
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                audioManager.abandonAudioFocusRequest(focusRequest);
-            else
-                audioManager.abandonAudioFocus(focusChangeListener);
-        }
+        // Abandon audio focus depending on device version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            audioManager.abandonAudioFocusRequest(focusRequest)
+        else
+            audioManager.abandonAudioFocus(focusChangeListener)
     }
 
-    private List<Word> createPhrasesCollection() {
-        List<Word> words = new ArrayList<>();
-        words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I'm feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I'm coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
-        words.add(new Word("I'm coming.", "әәnәm", R.raw.phrase_im_coming));
-        words.add(new Word("Let's go.", "yoowutis", R.raw.phrase_lets_go));
-        words.add(new Word("Come here.", "әnni'nem", R.raw.phrase_come_here));
-        return words;
+    private fun createPhrasesCollection(): List<Word> {
+        val words: MutableList<Word> = ArrayList()
+        words.add(Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going))
+        words.add(Word("What is your name?", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name))
+        words.add(Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is))
+        words.add(Word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling))
+        words.add(Word("I'm feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good))
+        words.add(Word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming))
+        words.add(Word("Yes, I'm coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming))
+        words.add(Word("I'm coming.", "әәnәm", R.raw.phrase_im_coming))
+        words.add(Word("Let's go.", "yoowutis", R.raw.phrase_lets_go))
+        words.add(Word("Come here.", "әnni'nem", R.raw.phrase_come_here))
+        return words
     }
 }
